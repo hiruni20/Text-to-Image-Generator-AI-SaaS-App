e8db35870d35f273c70049e3270ce06eb6f68a3e
Hiruni Nishara <hiruniranathunga14@gmail.com>
2025-10-03 23:13:54 +0530

    context provider

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { assets } from "../assets/assets"
 import { Link, useNavigate } from 'react-router-dom'
 
 
 const Navbar = () => {
 
-    const [user, setUser] = useState(null);
+    
     const navigate = useNavigate()
 
     return (

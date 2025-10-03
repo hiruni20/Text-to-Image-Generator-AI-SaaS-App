aeafc78592adcd8326b1cbc5c33c2c89037b15d9
Hiruni Nishara <hiruniranathunga14@gmail.com>
2025-10-03 22:48:08 +0530

    nav buttons

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
 import { assets } from "../assets/assets"
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 
 const Navbar = () => {
 
-    const [user, setUser] = useState(null);
+    const [user, setUser] = useState(true);
+    const navigate = useNavigate()
 
     return (
         <div className='flex item-center justify-between py-4'>
@@ -13,10 +14,24 @@ const Navbar = () => {
 
             <div>
                 {user ?
-                    <div></div>
+                    <div>
+                        <button>
+                            <img className='w-5' src={assets.credit_star} alt=''/>
+                            <p>Credits Left : 50</p>
+                        </button>
+                        <p>Hi, Hiruni</p>
+                        <div className='relative group'>
+                            <img src={assets.profile_icon} className='w-10 drop-shadow'alt=''/>
+                            <div className='absoulte hidden group-hover:block top-0 right-0 z-10 text-black rounded pt-12'>
+                                <ul>
+                                    <li>Logout</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                     :
                     <div className='flex items-center gap-2 sm:gap-5'>
-                        <p className='cursor-pointer'>Pricing</p> 
+                        <p onClick ={() =>navigate('/buy')}className='cursor-pointer'>Pricing</p> 
                         <button className='bg-zinc-800 text-white px-7 py-2 sm:px-2 text:sm rounded-full'>Loging</button>
                     </div>
                 }

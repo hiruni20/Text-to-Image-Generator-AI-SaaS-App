08e6c0093628607466c6b92fbe09e8d2cbd03ffb
Hiruni Nishara <hiruniranathunga14@gmail.com>
2025-10-04 19:13:55 +0530

    completed result page & buy pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b frm-teal-50 to-orange-50'>
+    <div className='px-4 sm:px-10 md:px-14 lg:px-28 min-h-screen bg-gradient-to-b from-teal-50 to-orange-50'>
       <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>} />

8d1c23f2df8cd664e96fb82a90e201b3bcb74f3a
Hiruni Nishara <hiruniranathunga14@gmail.com>
2025-10-05 13:21:36 +0530

    signup

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -11,23 +11,28 @@ const Login = () => {
             <h1 className='text-center text-2xl text-neutral-700
             font-medium'>Sign Up</h1>
             <p className='text-sm'>Welcome back! Please sign in to continue</p>
-            <div className='border px-6 flex items-center gap-2 
+            <div className='border px-6 py-2 flex items-center gap-2 
             rounded-full mt-5'>
                 <img  src={assets.email_icon} alt=""   />
                 <input className='outline-none text-sm'type='text' placeholder='Full Name' required/>
             </div>
 
-            <div className='border px-6 flex items-center gap-2 
-            rounded-full mt-5'>
+            <div className='border px-6 py-2 flex items-center gap-2 
+            rounded-full mt-4'>
                 <img src={assets.email_icon} alt="" />
                 <input className='outline-none text-sm'type='email' placeholder='Email id' required/>
             </div>
 
-            <div className='border px-6 flex items-center gap-2 
-            rounded-full mt-5'>
+            <div className='border px-6 py-2 flex items-center gap-2 
+            rounded-full mt-4'>
                 <img src={assets.lock_icon} alt="" />
                 <input className='outline-none text-sm'type='password' placeholder='Password' required/>
             </div>
+
+            <p className='text-sm text-blue-600 my-4 
+            cursor-pointer'>Forgot password</p>
+
+            <button className='bg-blue-600 w-full text-white py-2 rounded-full'>Create account</button>
         </form>
 
     </div>

f65270f61347aaaa75fc334d6f195bf28fada158
Hiruni Nishara <hiruniranathunga14@gmail.com>
2025-10-05 15:53:47 +0530

    login/sign up component

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,51 +1,74 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { assets } from '../assets/assets'
+import { AppContext } from '../context/AppContext'
 
 const Login = () => {
-  return (
-    <div className='absolute top-0 left-0 right-0 
+
+    const [state, setState] = useState('Login')
+    const {setShowLogin} = useContext(AppContext)
+    
+
+    
+    //scroll disable or not when login/signup form active
+    useEffect(()=>{
+
+        document.body.style.overflow = 'hidden';
+
+        return ()=>{
+            document.body.style.overflow = 'unset';
+        }
+
+    },[])
+    return (
+        <div className='absolute top-0 left-0 right-0 
     bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center'>
 
-        <form className='relative bg-white p-10 rounded-xl
+            <form className='relative bg-white p-10 rounded-xl
         text-slate-500'>
-            <h1 className='text-center text-2xl text-neutral-700
-            font-medium'>Sign Up</h1>
-            <p className='text-sm'>Welcome back! Please sign in to continue</p>
-            <div className='border px-6 py-2 flex items-center gap-2 
+                <h1 className='text-center text-2xl text-neutral-700
+            font-medium'>{state}</h1>
+                <p className='text-sm'>Welcome back! Please sign in to continue</p>
+
+                {state !== 'Login' &&
+                    <div className='border px-6 py-2 flex items-center gap-2 
             rounded-full mt-5'>
-                <img  src={assets.email_icon} alt=""   />
-                <input className='outline-none text-sm'type='text' placeholder='Full Name' required/>
-            </div>
+                        <img src={assets.email_icon} alt="" />
+                        <input className='outline-none text-sm' type='text' placeholder='Full Name' required />
+                    </div>
+                }
 
-            <div className='border px-6 py-2 flex items-center gap-2 
+                <div className='border px-6 py-2 flex items-center gap-2 
             rounded-full mt-4'>
-                <img src={assets.email_icon} alt="" />
-                <input className='outline-none text-sm'type='email' placeholder='Email id' required/>
-            </div>
+                    <img src={assets.email_icon} alt="" />
+                    <input className='outline-none text-sm' type='email' placeholder='Email id' required />
+                </div>
 
-            <div className='border px-6 py-2 flex items-center gap-2 
+                <div className='border px-6 py-2 flex items-center gap-2 
             rounded-full mt-4'>
-                <img src={assets.lock_icon} alt="" />
-                <input className='outline-none text-sm'type='password' placeholder='Password' required/>
-            </div>
+                    <img src={assets.lock_icon} alt="" />
+                    <input className='outline-none text-sm' type='password' placeholder='Password' required />
+                </div>
 
-            <p className='text-sm text-blue-600 my-4 
+                <p className='text-sm text-blue-600 my-4 
             cursor-pointer'>Forgot password</p>
 
-            <button className='bg-blue-600 w-full text-white py-2 rounded-full'>Create account</button>
+                <button className='bg-blue-600 w-full text-white py-2 rounded-full'>
+                    {state === 'Login' ? 'login' : 'Create account'}</button>
 
-            <p className='mt-5 text-center'>Don't have an account? 
-                <span className='text-blue-600 cursor-pointer'>Sign Up</span></p>
+                {state === 'Login' ? 
+                
+                    <p className='mt-5 text-center'>Don't have an account?
+                    <span className='text-blue-600 cursor-pointer' onClick={()=>setState('Sign Up')}>Sign Up</span></p>
+                    :
+                    <p className='mt-5 text-center'>Already have an account?
+                        <span className='text-blue-600 cursor-pointer' onClick={()=>setState('Login')}>Login</span></p>}
 
-                 <p className='mt-5 text-center'>Already have an account? 
-                <span className='text-blue-600 cursor-pointer'>Login</span></p>
-
-                <img src={assets.cross_icon} alt="" className='absolute top-5 right-5
+                <img onClick={()=>setShowLogin(false)}src={assets.cross_icon} alt="" className='absolute top-5 right-5
                 cursor-pointer'/>
-        </form>
+            </form>
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default Login
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { AppContext } from '../context/AppContext'
 
 const Navbar = () => {
 
-    const  {user } = useContext(AppContext)
+    const  {user,setShowLogin } = useContext(AppContext)
 
     
     const navigate = useNavigate()
@@ -36,7 +36,8 @@ const Navbar = () => {
                     :
                     <div className='flex items-center gap-2 sm:gap-5'>
                         <p onClick ={() =>navigate('/buy')}className='cursor-pointer'>Pricing</p> 
-                        <button className='bg-zinc-800 text-white px-7 py-2 sm:px-2 text:sm rounded-full'>Loging</button>
+                        <button className='bg-zinc-800 text-white px-7 py-2 sm:px-10 
+                        text:sm rounded-full' onClick={()=>setShowLogin(true)}>Loging</button>
                     </div>
                 }
 

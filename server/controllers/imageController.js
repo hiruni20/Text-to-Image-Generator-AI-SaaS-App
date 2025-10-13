686e2448e3faee93b5a46c346f89f90c19b147db
Hiruni Nishara <hiruniranathunga14@gmail.com>
2025-10-13 20:06:09 +0530

    api

--- server/controllers/imageController.js
+++ server/controllers/imageController.js
@@ -1,4 +1,6 @@
 import userModel from "../models/userModel"
+import FormData from "form-data"
+import axios from 'axios'
 
 
 const generate1image = async (req,res) => {
@@ -17,7 +19,20 @@ const generate1image = async (req,res) => {
         
         }
 
-    
+        const formData = new FormData()
+        formData.append('prompt', prompt)
+
+        const {data} = await axios.post('https://clipdrop-api.co/text-to-image/v1', formData, {
+            headers: {
+            'x-api-key': process.env.CLIPDROP_API,
+            },
+            responseType:'arraybuffer'
+        })
+
+        const base64Image = Buffer.from(data, 'binary').toString('base64')
+        const resultImage = `data:image/png;base64,${base64Image}`
+
+
     } catch (error) {
          console.log(error)
         res.json({success:false, message: error.message}) 
